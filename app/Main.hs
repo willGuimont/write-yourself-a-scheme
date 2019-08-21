ff0803300753933a8b12dfdc5b760a8e2bcc202c
willGuimont <wil9876543210@hotmail.com>
2019-08-20 21:35:35 -0400

    REPL

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,16 @@
 module Main where
 
 import Control.Monad.Except
-import Parsing
+import Data.String.Utils
+
 import System.Environment
+import System.IO
 import Text.ParserCombinators.Parsec
 
-import Evaluation
 import Types
 import Errors
+import Evaluation
+import Parsing
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input =
@@ -17,8 +20,32 @@ readExpr input =
     Left err -> throwError $ Parser err
     Right x -> return x
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = strip <$> (flushStr prompt >> getLine)
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (fmap show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ p prompt action = do
+  res <- prompt
+  if p res
+    then return ()
+    else action res >> until_ p prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "List>>> ") evalAndPrint
+
 main :: IO ()
 main = do
   args <- getArgs
-  let evaluated = fmap show $ readExpr (head args) >>= eval
-  putStrLn $ extractValue $ trapError evaluated
+  case Prelude.length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ strip $ Prelude.head args
+    _ -> putStrLn "Program takes only 0 or 1 argument"
